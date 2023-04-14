924ab51576a3eb35ced039cb6a93eaa669ef0a98
mouwadkp <mouhamed21227@gmail.com>
2023-04-13 23:24:56 -0400

    Ajout des classes Client et Hotel

---
+++ Client.cs
@@ -0,0 +1,50 @@
+﻿namespace ConsoleApp1
+{
+    internal class Client
+    {
+        //les proprietes de la classe client 
+
+        private string nom;
+        private string prenom;
+        private string courriel;
+        private string carteCredit;
+        private int numeroChambre;
+
+        public string Nom
+        {
+            get { return nom; }
+            set { nom = value; }
+        }
+        public string Prenom
+        {
+            get { return prenom; }
+            set { prenom = value; }
+        }
+        public string Courriel
+        {
+            get { return courriel; }
+            set { courriel = value; }
+        }
+        public string CarteCredit
+        {
+            get { return carteCredit; }
+            set { carteCredit = value; }
+        }
+        public int NumeroChambre
+        {
+            get { return numeroChambre; }
+            set { numeroChambre = value; }
+        }
+
+        public Client(string nom, string prenom, string courriel, string carteCredit, int numeroChambre)
+        {
+            this.Nom = nom;
+            this.Prenom = prenom;
+            this.Courriel = courriel;
+            this.CarteCredit = carteCredit;
+            this.NumeroChambre = numeroChambre;
+
+        }
+
+    }
+}
---
+++ Hotel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Hotel
+    {
+        //les proprietes de la classe hotel
+
+        private string nom;
+        private ArrayList clientsHotel;
+
+        //les ascensseur de la classe hotel
+        public String Nom
+        {
+            get { return nom; }
+            set { nom = value; }
+        }
+        public ArrayList ClientsHotel
+        {
+            get { return clientsHotel; }
+            set { clientsHotel = value; }
+        }
+
+        //les constructeur de la classe hotel
+        public Hotel(string nom)
+        {
+            Nom = nom;
+            clientsHotel = new ArrayList();
+        }
+    }
+}
